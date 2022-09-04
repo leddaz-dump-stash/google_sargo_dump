ac52077288573eac1900cebfdec8d2574c84b382
Leonardo Ledda <leonardoledda@gmail.com>
2022-09-04 20:26:02 +0000

    Add sargo-user 12 SP2A.220505.006 8561491 release-keys
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# sargo-user 12 SP2A.220505.006 8561491 release-keys
+- manufacturer: 
+- platform: sdm710
+- codename: sargo
+- flavor: sargo-user
+- release: 12
+- id: SP2A.220505.006
+- incremental: 8561491
+- tags: release-keys
+- fingerprint: google/sargo/sargo:12/SP2A.220505.006/8561491:user/release-keys
+- is_ab: true
+- brand: google
+- branch: sargo-user-12-SP2A.220505.006-8561491-release-keys
+- repo: google_sargo_dump
---
+++ twrp-device-tree/google/sargo/omni_sargo.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from sargo device
+$(call inherit-product, device/google/sargo/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sargo
+PRODUCT_NAME := omni_sargo
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3a
+PRODUCT_MANUFACTURER := google
